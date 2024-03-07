13f6f9db7f2b7cb44cb983f1abda122792633d39
Michał Haracewiat <m.haracewiat@gmail.com>
2024-03-07 18:36:23 +0100

    feat: auth resource stub

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from source.maps import bp as api_bp
+from source.auth import bp as auth_bp
+from source.maps import bp as maps_bp
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.register_blueprint(api_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(maps_bp)
 
     @app.route('/')
     def hello_geek():
---
+++ source/auth.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, jsonify
+
+
+bp = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
+
+@bp.route("/", methods=["POST"])
+def token():
+    return jsonify("dummy")
