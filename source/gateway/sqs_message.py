9a9e8bdd72cf33272c6fa2cb9b6f3bd2ef1e243a
Michał Haracewiat <m.haracewiat@gmail.com>
2024-03-10 17:02:37 +0100

    feat: sqs gateway

---
+++ source/gateway/sqs_message.py
@@ -0,0 +1,18 @@
+from source.gateway.message import MessageGateway
+from injector import inject
+from boto3 import client
+from source.model.map import Map
+
+
+class SqsMessageGateway(MessageGateway):
+
+    @inject
+    def __init__(self, queue_url: str, client: client):
+        self.queue_url = queue_url
+        self.client = client
+    
+    def notify(self, map: Map) -> None:
+        self.client.send_message(
+            QueueUrl=self.queue_url,
+            MessageBody=map.model_dump_json(),
+        )
---
+++ tests/gateway/test_sqs_message.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from unittest.mock import Mock
+from source.gateway.sqs_message import SqsMessageGateway
+from source.model.map import Map
+
+
+class TestSqsMessageGateway(TestCase):
+    
+    def test_success(self):
+        client_mock = Mock()
+        client_mock.send_message.return_value={}
+
+        queue_url = "http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/maps-uploaded"
+
+        sqs_message_gateway = SqsMessageGateway(
+            queue_url=queue_url,
+            client=client_mock, 
+        )
+
+        map = Map(id="123",file_path="/foo/bar/baz",is_processed=0,is_public=1)
+        sqs_message_gateway.notify(map)
+
+        client_mock.send_message.assert_called_once_with(
+            QueueUrl=queue_url,
+            MessageBody=map.model_dump_json(),
+        )
