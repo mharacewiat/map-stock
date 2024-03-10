3707e0f04a63da2f3d201c6bba359009fe80ea22
Michał Haracewiat <m.haracewiat@gmail.com>
2024-03-10 17:03:40 +0100

    chore: adjust dynamodb tables

--- localstack/ready.d/initialize.sh
+++ localstack/ready.d/initialize.sh
@@ -5,12 +5,16 @@ awslocal sqs create-queue \
 
 awslocal dynamodb create-table \
     --table-name users \
-    --billing-mode PAY_PER_REQUEST
-    --key-schema AttributeName=username,KeyType=HASH \
-    --attribute-definitions AttributeName=username,AttributeType=S
+    --billing-mode PAY_PER_REQUEST \
+    --attribute-definitions \
+        AttributeName=username,AttributeType=S \
+    --key-schema \
+        AttributeName=username,KeyType=HASH
 
 awslocal dynamodb create-table \
     --table-name maps \
-    --billing-mode PAY_PER_REQUEST
-    --key-schema AttributeName=id,KeyType=HASH \
-    --attribute-definitions AttributeName=id,AttributeType=S
+    --billing-mode PAY_PER_REQUEST \
+    --attribute-definitions \
+        AttributeName=id,AttributeType=S \
+    --key-schema \
+        AttributeName=id,KeyType=HASH
